417fb28eb14bed013ca8c412ed52444e0a9d53af
unknown <jeffpigg5@gmail.com>
2020-12-11 11:26:47 -0600

    Added DisplayWinner method to Games.cs, also started RunGame Method

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -9,36 +9,61 @@ namespace RPSLS
     class Game
     {
         // Memeber Variables (Has A)
-        public Player playerOne;
-        public Player playerTwo;
         public Player winner;
         public List<Gesture> gesturesList;
-        public List<Player> playersList;
+        public List<Player> playerList;
+        public Opponents opponents;
+        int playerOneScore;
+        int playerTwoScore;
+
 
         // Constructor (Spawner)
-        public Game(Player playerOne, Player playerTwo, Player winner)
-        {
-            this.playerOne = new Player(playerOne);
-            this.playerTwo = new Player(userInput);
+        public Game()
+        {           
+
         }
 
         // Member Methods (Can Do)
+        public void RunGame()
+        {
+            string playerOne = GetPlayersName();
+            string playerTwo = ChooseOpponent();
+
+            while(playerOneScore < 2 && playerTwoScore < 2)
+            {
+                // Run  single round of the game
+            }
+
+            DisplayWinner();
+        }
+
         public string GetPlayersName()
         {
             Console.WriteLine("What is your name?");
             string playersName = Console.ReadLine();
             return playersName;
         }
-        public int ChooseOpponent(Opponents opponents)
+        public string ChooseOpponent()
         {
             Console.WriteLine("Choose your opponent:");
             for (int i = 0; i < opponents.playerList.Count; i++)
             {
-                Console.WriteLine("Press " + i + " for " + opponents.playerList[i].name);
+                Console.WriteLine("Press " + i + " for " + opponents.playerList[i].type);
             }
             string input = Console.ReadLine();
-            return Int32.Parse(input);
+            return input;
         }
-        
+        public void DisplayWinner()
+        {
+            if (playerOneScore > playerTwoScore)
+            {
+                Console.WriteLine("Player One has won the game!");
+            }
+
+            else
+            {
+                Console.WriteLine("Player Two has won the game!");
+            }
+        }
     }
 }
