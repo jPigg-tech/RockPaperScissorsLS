328b34151388320523acfa0cfb75805437723db3
unknown <jeffpigg5@gmail.com>
2020-12-11 10:39:55 -0600

    Added ChooseOpponent method to Game.cs

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -13,19 +13,32 @@ namespace RPSLS
         public Player playerTwo;
         public Player winner;
         public List<Gesture> gesturesList;
+        public List<Player> playersList;
 
         // Constructor (Spawner)
         public Game(Player playerOne, Player playerTwo, Player winner)
         {
-            this.playerOne = new Player(userInput);
+            this.playerOne = new Player(playerOne);
             this.playerTwo = new Player(userInput);
         }
 
         // Member Methods (Can Do)
-        public string GetPlayerOnesName()
+        public string GetPlayersName()
         {
             Console.WriteLine("What is your name?");
-            Console.ReadLine(string userInput);
+            string playersName = Console.ReadLine();
+            return playersName;
         }
+        public int ChooseOpponent(Opponents opponents)
+        {
+            Console.WriteLine("Choose your opponent:");
+            for (int i = 0; i < opponents.playerList.Count; i++)
+            {
+                Console.WriteLine("Press " + i + " for " + opponents.playerList[i].name);
+            }
+            string input = Console.ReadLine();
+            return Int32.Parse(input);
+        }
+        
     }
 }
